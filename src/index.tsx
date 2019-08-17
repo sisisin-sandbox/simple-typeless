9ac17305c33a3b9d7c31cd5309b7d5880024b6cc
sisisin <azsisisin@gmail.com>
2019-08-17 19:33:02 +0900

    using promise

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,12 @@ import * as Rx from 'typeless/rx';
 
 const CounterSymbol = Symbol('counter');
 
-const [useCounterModule, CounterActions, getCounterState] = createModule(CounterSymbol)
+const [useCounterModule, SomeActions, getCounterState] = createModule(CounterSymbol)
   .withActions({
     increment: null,
-    decrement: null,
-    reset: null,
-    alertCount: null,
+    fetchSomebody: null,
+    fetchSomebodyFulfilled: (some: { id: number; somebody: string }) => ({ payload: some }),
+    rejected: null,
   })
   .withState<CounterState>();
 
@@ -18,38 +18,50 @@ interface CounterState {
   count: number;
 }
 
-useCounterModule.epic().on(CounterActions.alertCount, () => {
-  const { count } = getCounterState();
-  alert(`count: ${count}`);
-  return Rx.empty();
-});
+const API = {
+  getSomeId: () => Promise.resolve(42),
+  getSomebodyById: (id: number) => Promise.resolve('somebody'),
+};
+
 useCounterModule
-  .reducer({ count: 0 })
-  .on(CounterActions.increment, state => {
-    state.count++;
+  .epic()
+  .on(SomeActions.fetchSomebody, () => {
+    return Rx.from(API.getSomeId()).pipe(
+      Rx.mergeMap(id =>
+        Rx.from(API.getSomebodyById(id)).pipe(
+          Rx.map(somebody => SomeActions.fetchSomebodyFulfilled({ id, somebody })),
+        ),
+      ),
+      Rx.catchError(e => Rx.of(SomeActions.rejected())),
+    );
   })
-  .on(CounterActions.decrement, state => {
-    state.count--;
-  })
-  .replace(CounterActions.reset, () => {
-    return { count: 0 };
-  });
+  .on(SomeActions.fetchSomebody, (async () => {
+    try {
+      const id = await API.getSomeId();
+      const somebody = await API.getSomebodyById(id);
+      return SomeActions.fetchSomebodyFulfilled({ id, somebody });
+    } catch (e) {
+      return SomeActions.rejected();
+    }
+  }) as any)
+  .on(SomeActions.fetchSomebody, (async () => {
+    return Rx.empty();
+  }) as any)
+useCounterModule.reducer({ count: 0 }).on(SomeActions.increment, state => {
+  state.count++;
+});
 
 function Main() {
   useCounterModule();
 
-  const { alertCount, reset, increment, decrement } = useActions(CounterActions);
+  const { increment, fetchSomebody: rejectApi } = useActions(SomeActions);
   const { count } = getCounterState.useState();
   return (
     <div>
-      <button onClick={decrement}>-</button>
       <div>{count}</div>
       <button onClick={increment}>+</button>
       <div>
-        <button onClick={reset}>reset!</button>
-      </div>
-      <div>
-        <button onClick={alertCount}>alert!</button>
+        <button onClick={rejectApi}>alert!</button>
       </div>
     </div>
   );
